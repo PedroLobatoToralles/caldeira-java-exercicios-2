4f43fb5b99f4a1043e95e8c92dfdec13f79f2617
Pedro Lobato Toralles <134618293+PedroLobatoToralles@users.noreply.github.com>
2023-10-03 16:52:35 -0300

    aula 1 de outubro

---
+++ exercicios-aula-05/a5e1/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insira um número inteiro:");
+        System.out.println("Mais um:");
+        System.out.println("Mais outro:");
+        System.out.println("Pode dale mais um");
+        System.out.println("Só mais um ultimo:");
+
+
+
+    }
+}
